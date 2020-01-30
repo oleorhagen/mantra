116f868c33110d4197f64f6033ae6b413929df5c
Lluis Campos <lluis.campos@northern.tech>
2020-01-30 10:16:23 +0100

    Finilize the helper scripts to fetch/post all test suites
    The scripts have been basically rewritten. There are still few things
    hardcoded (hostname, for example) but they are fully functional now
    fetching and posting the results for all test suites of Mender QA.
    
    Changelog: None
    
    Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

--- scripts/get-nightly-gitlab.py
+++ scripts/get-nightly-gitlab.py
@@ -7,17 +7,14 @@ For the last 10 days, get the xml results available in GitLab"""
 import requests
 import os
 import sys
-import logging
 from datetime import timedelta, date
 
+from common import logger
 from common import TEST_RESULTS_DIR
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("tetrautils")
-logger.setLevel(logging.INFO)
+from common import MENDER_QA_TEST_SUITES
 
 # Get token from env
-token = os.getenv('GITLAB_TOKEN')
+token = os.getenv("GITLAB_TOKEN")
 if token is None:
     logger.error("GITLAB_TOKEN not found in user environment")
     sys.exit(1)
@@ -25,66 +22,99 @@ if token is None:
 base_url = "https://gitlab.com/api/v4/projects/12501706/"
 pipelines_api = base_url + "pipelines/"
 jobs_api = base_url + "jobs/"
+artifacts_api_fmt = base_url + "jobs/{job_id}/artifacts/{artifact_filename}"
 
-def iterate_dates(start_date, delta_days=-1, total_days=-10):
+
+def iterate_dates(start_date, total_days, delta_days=-1):
     for delta in range(0, total_days, delta_days):
         yield start_date + timedelta(days=delta)
 
-def iterate_nightlies(total_days=-10):
+
+def iterate_nightlies(start_date, total_days):
     # Filter with user that triggers master and nightlies:
     parameters_base = "username=lluiscampos"
 
-    for single_date in iterate_dates(date.today(), total_days):
+    for single_date in iterate_dates(start_date, total_days):
         single_date_str = single_date.strftime("%Y-%m-%d")
         logger.info("Looking for nightly " + single_date_str)
-        for end_hour in range(2, 8):
-            parameters = parameters_base + "&updated_after={}-{:02}-{:02}T01:00:00Z".format(
-                single_date.year, single_date.month, single_date.day)
+        for end_hour in range(2, 12):
+            parameters = (
+                parameters_base
+                + "&updated_after={}-{:02}-{:02}T01:00:00Z".format(
+                    single_date.year, single_date.month, single_date.day
+                )
+            )
             parameters += "&updated_before={}-{:02}-{:02}T:{:02}:00:00Z".format(
-                single_date.year, single_date.month, single_date.day, end_hour)
+                single_date.year, single_date.month, single_date.day, end_hour
+            )
             url = "{url}?{params}".format(url=pipelines_api, params=parameters)
             logger.debug("Fetching URL: " + url)
-            r = requests.get(url, headers={'PRIVATE-TOKEN': token})
+            r = requests.get(url, headers={"PRIVATE-TOKEN": token})
             j = r.json()
             logger.debug("Got JSON: " + str(j))
             if len(j) > 0:
                 nightly_id = j[0]["id"]
-                logger.info("Found nightly " + str(nightly_id)  + " for " + single_date_str)
+                logger.info(
+                    "Found nightly " + str(nightly_id) + " for " + single_date_str
+                )
                 yield single_date_str, j[0]["id"]
                 break
         else:
             logger.error("Could not find nightly for " + single_date_str)
 
-def fetch_and_save_last_10_nightlies():
-    for date_str, nightly_id in iterate_nightlies(-10):
 
-        #TODO CHECK NIGHTHLY HAS NO VARIABLES
-
+def fetch_and_save_nightlies(start_date=date.today(), total_days=-5):
+    for date_str, nightly_id in iterate_nightlies(start_date, total_days):
         url = pipelines_api + str(nightly_id) + "/jobs"
         logger.debug("Fetching URL: " + url)
-        r = requests.get(url, headers={'PRIVATE-TOKEN': token})
+        r = requests.get(url, headers={"PRIVATE-TOKEN": token})
         j = r.json()
         logger.debug("Got JSON: " + str(j))
 
-        integ_tests_job = [jj["id"] for jj in j if jj["name"] == "test_full_integration"]
-        if len(integ_tests_job) == 0:
-            jobs = [jj["name"] for jj in j]
-            logger.error("Cannot fine test_full_integration in job list. Got: " + ",". join(jobs) )
+        for project in MENDER_QA_TEST_SUITES:
+            test_job = [jj["id"] for jj in j if jj["name"] == project["name"]]
+            if len(test_job) == 0:
+                # RPi is not build by default
+                if project["name"] == "test_accep_raspberrypi3":
+                    logger_func = logger.warning
+                else:
+                    logger_func = logger.error
+                logger_func("Cannot find %s in job list" % project["name"])
+                continue
 
-        integ_tests_job = integ_tests_job[0]
-        logger.info("Fetching XML results for test_full_integration")
-        url = jobs_api + str(integ_tests_job) + "/artifacts/results_full_integration.xml"
-        logger.debug("Fetching URL: " + url)
-        r = requests.get(url, headers={'PRIVATE-TOKEN': token})
+            test_job = test_job[0]
+            logger.info("Fetching XML results for %s" % project["name"])
+            url = artifacts_api_fmt.format(
+                job_id=test_job, artifact_filename=project["results_file"] + ".xml"
+            )
+            logger.debug("Fetching URL: " + url)
+            r = requests.get(url, headers={"PRIVATE-TOKEN": token})
 
-        filename = os.path.join(TEST_RESULTS_DIR, "results_full_integration" + date_str + ".xml")
-        if not os.path.exists(filename):
-            logger.info("Saving report in " + filename)
-            with open(filename, "wb") as fd:
-                fd.write(r.content)
-        else:
-            logger.info("Report " + filename + " already exists, skipping")
+            if not r.ok:
+                # BBB and RPi are not tested
+                if project["name"] in [
+                    "test_accep_beagleboneblack",
+                    "test_accep_raspberrypi3",
+                ]:
+                    logger_func = logger.warning
+                else:
+                    logger_func = logger.error
+                logger_func(
+                    "Cannot get results file %s from %s "
+                    % (project["results_file"], project["name"])
+                )
+                continue
+
+            filename = os.path.join(
+                TEST_RESULTS_DIR, project["results_file"] + "@" + date_str + ".xml"
+            )
+            if not os.path.exists(filename):
+                logger.info("Saving report in " + filename)
+                with open(filename, "wb") as fd:
+                    fd.write(r.content)
+            else:
+                logger.warning("Report " + filename + " already exists, skipping")
 
 
 if __name__ == "__main__":
-    fetch_and_save_last_10_nightlies()
+    fetch_and_save_nightlies()
--- scripts/init-tetra-projects.py
+++ scripts/init-tetra-projects.py
@@ -6,52 +6,37 @@ import requests
 from requests.auth import HTTPBasicAuth
 import os
 import sys
-import logging
 import json
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("tetrautils")
-logger.setLevel(logging.INFO)
+from common import logger
+from common import MENDER_QA_TEST_SUITES
+from common import TETRA_API_PROJECTS_URL
+from common import get_tetra_credentials
 
-MENDER_QA_TEST_SUITES = [
-    "test_accep_qemux86_64_uefi_grub",
-    "test_accep_vexpress_qemu",
-    "test_accep_qemux86_64_bios_grub",
-    "test_accep_qemux86_64_bios_grub_gpt",
-    "test_accep_vexpress_qemu_uboot_uefi_grub",
-    "test_accep_vexpress_qemu_flash",
-    "test_accep_beagleboneblack",
-    "test_accep_raspberrypi3",
-    "test_backend_integration",
-    "test_full_integration"
-]
+user, password = get_tetra_credentials()
 
-# Get token from env
-user = os.getenv('TETRA_USER')
-password = os.getenv('TETRA_PASSWORD')
-if user is None or password is None:
-    logger.error("TETRA_USER or TETRA_PASSWORD not found in user environment")
-    sys.exit(1)
-
-base_url = "http://localhost/api/"
-projects_api = base_url + "projects"
-
-r = requests.get(projects_api, auth=HTTPBasicAuth(user, password))
+r = requests.get(TETRA_API_PROJECTS_URL, auth=HTTPBasicAuth(user, password))
 j = r.json()
 if len(j) > 0:
-    logger.warning("The following rojects already exist:")
+    logger.warning("The following projects already exist:")
     logger.warning(j)
     logger.warning("Exiting")
     sys.exit(0)
 
 for project in MENDER_QA_TEST_SUITES:
-    logger.debug("Creating project %s" % project)
-    r = requests.post(projects_api,
-            auth=HTTPBasicAuth(user, password),
-            data=json.dumps({"name": project}))
-    # import pdb; pdb.set_trace()
+    logger.debug("Creating project %s" % project["name"])
+    r = requests.post(
+        TETRA_API_PROJECTS_URL,
+        auth=HTTPBasicAuth(user, password),
+        data=json.dumps({"name": project["name"]}),
+    )
     if not r.ok:
         logger.error("Error(%d) %s" % (r.status_code, r.text))
         sys.exit(1)
     j = r.json()
+
+    logger.debug("Got JSON: %s" % str(j))
+    if j["id"] != project["id"]:
+        logger.error("Expected id %d, got %d" % (project["id"], j["id"]))
+        sys.exit(1)
     logger.info("Created project: %s" % j)
