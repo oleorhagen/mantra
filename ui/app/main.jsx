0a314612e31926ab0edec3c664413a0daf8e4edd
Manuel Zedel <manuel.zedel@northern.tech>
2022-04-05 09:45:48 +0200

    chore: updated ui packages & setup
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- ui/app/main.jsx
+++ ui/app/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProjectsView from './projects-view';
 import BuildsView from './builds-view';
 import ResultsView from './results-view';
@@ -8,14 +8,14 @@ import LastFailedView from './last-failed-view';
 import HistoryView from './history-view';
 
 render(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/projects/:project_id/builds/:build_name/last_failed/:count" component={LastFailedView} />
-            <Route path="/projects/:project_id/builds/:build_id/results" component={ResultsView} />
-            <Route path="/projects/:project_id/builds" component={BuildsView} />
-            <Route path="/projects/:project_id/tests/:test_name/history/:count" component={HistoryView} />
-            <Route exact path="/" component={ProjectsView} />
-        </Switch>
-    </BrowserRouter>,
-    document.getElementById('content')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/projects/:project_id/builds/:build_name/last_failed/:count" element={LastFailedView} />
+      <Route path="/projects/:project_id/builds/:build_id/results" element={ResultsView} />
+      <Route path="/projects/:project_id/builds" element={BuildsView} />
+      <Route path="/projects/:project_id/tests/:test_name/history/:count" element={HistoryView} />
+      <Route exact path="/" element={ProjectsView} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('content')
 );
