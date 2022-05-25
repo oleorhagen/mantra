a885d38b0b6703d13f625e20b321b14cb6263313
Ole Petter <ole.orhagen@northern.tech>
2022-05-25 19:42:20 +0200

    Start using the DataGrid for all project tables
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/resource-table.js
+++ ui/src/resource-table.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
-import ResultsTable from './results-table.js';
+import { DataGrid, GridToolbar, LinearProgress, CustomNoRowsOverlay } from '@mui/x-data-grid';
 
 import ResourceTableEntry from './resource-table-entry';
 
@@ -14,8 +12,8 @@ const tableTypes = {
     keys: ['id', 'name'],
     links: {
       name: project => `/projects/${project.id}/builds`,
-      id: project => `/projects/${project.id}/builds`
-    }
+      id: project => `/projects/${project.id}/builds`,
+    },
   },
   builds: {
     titles: ['Id', 'Project Id', 'Name', 'Build Url', 'Region', 'Environment', 'Show Failed'],
@@ -23,37 +21,137 @@ const tableTypes = {
     links: {
       name: build => `/projects/${build.project_id}/builds/${build.id}`,
       id: build => `/projects/${build.project_id}/builds/${build.id}`,
-      show_failed: build => `/projects/${build.project_id}/builds/last_failed/?name=${encodeURIComponent(build.name)}&count=10`
-    }
-  }
+      show_failed: build => `/projects/${build.project_id}/builds/last_failed/?name=${encodeURIComponent(build.name)}&count=10`,
+    },
+  },
+};
+
+const tableColumnDefinitions = {
+  projects: [
+    {
+      field: 'id',
+      headerName: 'ID',
+      minWidth: 100,
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      minWidth: 250,
+    },
+  ],
+  results: [
+    {
+      field: 'id',
+      headerName: 'Build ID',
+      minWidth: 150,
+      sortable: false,
+      renderCell: params => <Link href={`/projects/${params.row.project_id}/builds/${params.row.id}`}>{params.row.id}</Link>,
+      editable: false,
+    },
+    {
+      field: 'project_id',
+      headerName: 'Project ID',
+      minWidth: 150,
+      sortable: false,
+      filterable: false,
+      renderCell: params => <Link href={`/projects/${params.row.project_id}/builds`}>{params.row.project_id}</Link>,
+      editable: false,
+    },
+    {
+      field: 'result',
+      headerName: 'Result',
+      minWidth: 110,
+      renderCell: params => {
+        var color = 'green';
+        if (params.row.result != 'passed') {
+          color = 'red';
+        }
+        return <div style={{ color: color }}>{params.row.result}</div>;
+      },
+      sortable: true,
+    },
+    {
+      field: 'result_message',
+      headerName: 'ResultMessage',
+      sortable: false,
+      minWidth: 110,
+    },
+    {
+      field: 'test_name',
+      headerName: 'Test Name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: true,
+      minMinWidth: 260,
+      flex: 1,
+      valueGetter: params => `/projects/${params.row.project_id}/tests/history?name=${encodeURIComponent(params.row.test_name)}&count=10`,
+      renderCell: params => (
+        <Link href={`/projects/${params.row.project_id}/tests/history?name=${encodeURIComponent(params.row.test_name)}&count=10`}>{params.row.test_name}</Link>
+      ),
+    },
+    {
+      field: 'tags',
+      headerName: 'Tags',
+      sortable: true,
+      renderCell: params => {
+        if (params.row.tags.nightly) {
+          return <div style={{ color: 'orange' }}>nightly</div>;
+        }
+        return '';
+      },
+      minWidth: 100,
+    },
+    {
+      field: 'timestamp',
+      headerName: 'TimeStamp',
+      sortable: true,
+      valueGetter: params => {
+        const date = new Date(params.row.timestamp * 1000);
+        return date.toUTCString();
+      },
+      minWidth: 200,
+    },
+  ],
+  builds: {},
 };
 
 const ResourceTable = ({ resources, type }) => {
   console.log('Resources:');
   console.log(resources);
-  // TODO - != -> ===
-  if (type != 'results') {
-    return ResultsTable(resources);
-  }
-  const { titles: columnTitles, keys: columnKeys, links: columnLinks } = tableTypes[type];
+  console.log('type');
+  console.log(type);
+
+  const columns = tableColumnDefinitions[type];
+
+  const rows = resources;
+
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            {columnTitles.map((title, index) => (
-              <TableCell key={index}>{title}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {resources.map((resource, i) => (
-            <ResourceTableEntry key={i} resource={resource} columnKeys={columnKeys} columnLinks={columnLinks} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <DataGrid
+      autoHeight
+      showQuickFilter Adds a general search text input field
+      initialState={{
+        sorting: {
+          sortModel: [{ field: 'result', sort: 'asc' }],
+        },
+      }}
+      rows={rows}
+      columns={columns}
+      pageSize={50}
+      rowsPerPageOptions={[50]}
+      disableSelectionOnClick
+      disableColumnSelector
+      disableDensitySelector
+      components={{
+        Toolbar: GridToolbar,
+        LoadingOverlay: LinearProgress,
+        NoRowsOverlay: CustomNoRowsOverlay,
+      }}
+      componentsProps={{
+        toolbar: {
+          showQuickFilter: true, // Quero no worko?
+          quickFilterProps: { debounceMs: 500 },
+        },
+      }}
+    />
   );
 };
 
