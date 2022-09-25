26b207eb00b51c0516c1653df2dcd26bc939e04f
Ole Petter <ole.orhagen@northern.tech>
2022-09-25 17:54:12 +0200

    start creating the new table format
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- backend/sql/001_init.sql
+++ backend/sql/001_init.sql
@@ -1,5 +1,23 @@
 
 
-CREATE TABLE IF NOT EXISTS test (
-    id SERIAL8 PRIMARY KEY
+CREATE TABLE IF NOT EXISTS jobs (
+    id SERIAL8 PRIMARY KEY,
+    pipeline_id SERIAL8 FOREIGN KEY REFERENCES pipelines.id,
+    _name STRING,
+    status STRING,
+);
+
+CREATE TABLE IF NOT EXISTS pipelines (
+    id SERIAL8 PRIMARY KEY,
+    _name STRING,
+    status STRING,
+);
+
+CREATE TABLE IF NOT EXISTS results (
+    job_id SERIAL8 PRIMARY KEY,
+    _name STRING,
+    status STRING,
+    t TIMESTAMP,
+    _result STRING,
+    result_message STRING
 );
