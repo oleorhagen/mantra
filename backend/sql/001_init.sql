5701ddf307dc7b58b84b5f1f1af646f83fdf2a62
Ole Petter <ole.orhagen@northern.tech>
2022-10-05 12:25:10 +0200

    reformat the table init schema
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- backend/sql/001_init.sql
+++ backend/sql/001_init.sql
@@ -1,23 +1,29 @@
 
-
-CREATE TABLE IF NOT EXISTS jobs (
-    id SERIAL8 PRIMARY KEY,
-    pipeline_id SERIAL8 FOREIGN KEY REFERENCES pipelines.id,
-    _name STRING,
+CREATE TABLE IF NOT EXISTS pipelines (
+    id SERIAL PRIMARY KEY,
+    _name STRING NOT NULL,
+    time_start TIMESTAMP,
+    time_end TIMESTAMP,
     status STRING,
 );
 
-CREATE TABLE IF NOT EXISTS pipelines (
-    id SERIAL8 PRIMARY KEY,
-    _name STRING,
+CREATE TABLE IF NOT EXISTS jobs (
+    id SERIAL PRIMARY KEY,
+    pipeline_id SERIAL NOT NULL,
+    _name STRING NOT NULL,
     status STRING,
+    FOREIGN KEY(pipeline_id)
+        REFERENCES pipelines (id)
 );
 
 CREATE TABLE IF NOT EXISTS results (
-    job_id SERIAL8 PRIMARY KEY,
-    _name STRING,
-    status STRING,
-    t TIMESTAMP,
-    _result STRING,
-    result_message STRING
+    id SERIAL PRIMARY KEY,
+    job_id SERIAL NOT NULL,
+    _name STRING NOT NULL,
+    status STRING NOT NULL,
+    time_start TIMESTAMP,
+    time_end TIMESTAMP,
+    result_message STRING,
+    FOREIGN KEY(job_id)
+        REFERENCES jobs (id)
 );
