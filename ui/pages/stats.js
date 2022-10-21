60df71d9ac5758e609c2c591bfeb37dac759f7b0
Ole Petter <ole.orhagen@northern.tech>
2022-10-21 12:23:35 +0200

    feat: Add a simple graph showing the number of failures per day
    Changelog: None
    Ticket: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/pages/stats.js
+++ ui/pages/stats.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Paper } from '@mui/material';
 
-import SpuriousFailuresView from '../src/statistics/spurious-failures-view.js';
+import SpuriousFailuresView from '../src/statistics/spurious-failures-view';
+import PlotView from '../src/statistics/plot-view';
 
 const Stats = props => {
   return (
     <Paper elevation={0}>
       <SpuriousFailuresView />
+      <PlotView />
     </Paper>
   );
 };
